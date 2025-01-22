2d61ab09e058aee4188037c7cc74c6ba8da4f8bc
Shawn <sgiovanni1@yahoo.com>
2025-01-22 07:05:58 -0500

    got keyboard working

---
+++ react-vite/src/components/MidiKeyboardPage/images.js
@@ -0,0 +1,203 @@
+//This file contains all the SVG dist to be used for the
+console.log("dist is working")
+
+export default class KeyImages {
+    releasedNatural = [
+        "../dist/svg/released_natural/C_naturalR.svg",
+        "../dist/svg/released_natural/D_naturalR.svg",
+        "../dist/svg/released_natural/E_naturalR.svg",
+        "../dist/svg/released_natural/F_naturalR.svg",
+        "../dist/svg/released_natural/G_naturalR.svg",
+        "../dist/svg/released_natural/A_naturalR.svg",
+
+        "../dist/svg/released_natural/B_naturalR.svg",
+        "../dist/svg/released_natural/C_naturalR.svg",
+        "../dist/svg/released_natural/D_naturalR.svg",
+        "../dist/svg/released_natural/E_naturalR.svg",
+        "../dist/svg/released_natural/F_naturalR.svg",
+        "../dist/svg/released_natural/G_naturalR.svg",
+        "../dist/svg/released_natural/A_naturalR.svg",
+
+        "../dist/svg/released_natural/B_naturalR.svg",
+        "../dist/svg/released_natural/C_naturalR.svg",
+        "../dist/svg/released_natural/D_naturalR.svg",
+        "../dist/svg/released_natural/E_naturalR.svg",
+        "../dist/svg/released_natural/F_naturalR.svg",
+        "../dist/svg/released_natural/G_naturalR.svg",
+        "../dist/svg/released_natural/A_naturalR.svg",
+
+        "../dist/svg/released_natural/B_naturalR.svg",
+        "../dist/svg/released_natural/C_naturalR.svg",
+        "../dist/svg/released_natural/D_naturalR.svg",
+        "../dist/svg/released_natural/E_naturalR.svg",
+        "../dist/svg/released_natural/F_naturalR.svg",
+        "../dist/svg/released_natural/G_naturalR.svg",
+        "../dist/svg/released_natural/A_naturalR.svg",
+
+        "../dist/svg/released_natural/B_naturalR.svg",
+        "../dist/svg/released_natural/C_naturalR.svg",
+        "../dist/svg/released_natural/D_naturalR.svg",
+        "../dist/svg/released_natural/E_naturalR.svg",
+        "../dist/svg/released_natural/F_naturalR.svg",
+        "../dist/svg/released_natural/G_naturalR.svg",
+        "../dist/svg/released_natural/A_naturalR.svg",
+
+        "../dist/svg/released_natural/B_naturalR.svg",
+        "../dist/svg/released_natural/C_naturalR.svg",
+        "../dist/svg/released_natural/D_naturalR.svg",
+        "../dist/svg/released_natural/E_naturalR.svg",
+        "../dist/svg/released_natural/F_naturalR.svg",
+        "../dist/svg/released_natural/G_naturalR.svg",
+        "../dist/svg/released_natural/A_naturalR.svg",
+
+        "../dist/svg/released_natural/B_naturalR.svg",
+        "../dist/svg/released_natural/C_naturalR.svg",
+        "../dist/svg/released_natural/D_naturalR.svg",
+        "../dist/svg/released_natural/E_naturalR.svg",
+        "../dist/svg/released_natural/F_naturalR.svg",
+        "../dist/svg/released_natural/G_naturalR.svg",
+        "../dist/svg/released_natural/A_naturalR.svg",
+        "../dist/svg/released_natural/B_naturalR.svg",
+
+    ]
+    pressedNatural = [
+        "../dist/svg/pressed_natural/C_naturalP.svg",
+        "../dist/svg/pressed_natural/D_naturalP.svg",
+        "../dist/svg/pressed_natural/E_naturalP.svg",
+        "../dist/svg/pressed_natural/F_naturalP.svg",
+        "../dist/svg/pressed_natural/G_naturalP.svg",
+        "../dist/svg/pressed_natural/A_naturalP.svg",
+        "../dist/svg/pressed_natural/B_naturalP.svg",
+
+        "../dist/svg/pressed_natural/C_naturalP.svg",
+        "../dist/svg/pressed_natural/D_naturalP.svg",
+        "../dist/svg/pressed_natural/E_naturalP.svg",
+        "../dist/svg/pressed_natural/F_naturalP.svg",
+        "../dist/svg/pressed_natural/G_naturalP.svg",
+        "../dist/svg/pressed_natural/A_naturalP.svg",
+        "../dist/svg/pressed_natural/B_naturalP.svg",
+
+        "../dist/svg/pressed_natural/C_naturalP.svg",
+        "../dist/svg/pressed_natural/D_naturalP.svg",
+        "../dist/svg/pressed_natural/E_naturalP.svg",
+        "../dist/svg/pressed_natural/F_naturalP.svg",
+        "../dist/svg/pressed_natural/G_naturalP.svg",
+        "../dist/svg/pressed_natural/A_naturalP.svg",
+        "../dist/svg/pressed_natural/B_naturalP.svg",
+
+        "../dist/svg/pressed_natural/C_naturalP.svg",
+        "../dist/svg/pressed_natural/D_naturalP.svg",
+        "../dist/svg/pressed_natural/E_naturalP.svg",
+        "../dist/svg/pressed_natural/F_naturalP.svg",
+        "../dist/svg/pressed_natural/G_naturalP.svg",
+        "../dist/svg/pressed_natural/A_naturalP.svg",
+        "../dist/svg/pressed_natural/B_naturalP.svg",
+
+        "../dist/svg/pressed_natural/C_naturalP.svg",
+        "../dist/svg/pressed_natural/D_naturalP.svg",
+        "../dist/svg/pressed_natural/E_naturalP.svg",
+        "../dist/svg/pressed_natural/F_naturalP.svg",
+        "../dist/svg/pressed_natural/G_naturalP.svg",
+        "../dist/svg/pressed_natural/A_naturalP.svg",
+        "../dist/svg/pressed_natural/B_naturalP.svg",
+
+        "../dist/svg/pressed_natural/C_naturalP.svg",
+        "../dist/svg/pressed_natural/D_naturalP.svg",
+        "../dist/svg/pressed_natural/E_naturalP.svg",
+        "../dist/svg/pressed_natural/F_naturalP.svg",
+        "../dist/svg/pressed_natural/G_naturalP.svg",
+        "../dist/svg/pressed_natural/A_naturalP.svg",
+        "../dist/svg/pressed_natural/B_naturalP.svg",
+
+        "../dist/svg/pressed_natural/C_naturalP.svg",
+        "../dist/svg/pressed_natural/D_naturalP.svg",
+        "../dist/svg/pressed_natural/E_naturalP.svg",
+        "../dist/svg/pressed_natural/F_naturalP.svg",
+        "../dist/svg/pressed_natural/G_naturalP.svg",
+        "../dist/svg/pressed_natural/A_naturalP.svg",
+        "../dist/svg/pressed_natural/B_naturalP.svg",
+    ]
+    releasedSharp = [
+        "../dist/svg/released_sharp/C_sharpR.svg",
+        "../dist/svg/released_sharp/D_sharpR.svg",
+        "../dist/svg/released_sharp/F_sharpR.svg",
+        "../dist/svg/released_sharp/G_sharpR.svg",
+        "../dist/svg/released_sharp/A_sharpR.svg",
+
+        "../dist/svg/released_sharp/C_sharpR.svg",
+        "../dist/svg/released_sharp/D_sharpR.svg",
+        "../dist/svg/released_sharp/F_sharpR.svg",
+        "../dist/svg/released_sharp/G_sharpR.svg",
+        "../dist/svg/released_sharp/A_sharpR.svg",
+
+        "../dist/svg/released_sharp/C_sharpR.svg",
+        "../dist/svg/released_sharp/D_sharpR.svg",
+        "../dist/svg/released_sharp/F_sharpR.svg",
+        "../dist/svg/released_sharp/G_sharpR.svg",
+        "../dist/svg/released_sharp/A_sharpR.svg",
+
+        "../dist/svg/released_sharp/C_sharpR.svg",
+        "../dist/svg/released_sharp/D_sharpR.svg",
+        "../dist/svg/released_sharp/F_sharpR.svg",
+        "../dist/svg/released_sharp/G_sharpR.svg",
+        "../dist/svg/released_sharp/A_sharpR.svg",
+
+        "../dist/svg/released_sharp/C_sharpR.svg",
+        "../dist/svg/released_sharp/D_sharpR.svg",
+        "../dist/svg/released_sharp/F_sharpR.svg",
+        "../dist/svg/released_sharp/G_sharpR.svg",
+        "../dist/svg/released_sharp/A_sharpR.svg",
+
+        "../dist/svg/released_sharp/C_sharpR.svg",
+        "../dist/svg/released_sharp/D_sharpR.svg",
+        "../dist/svg/released_sharp/F_sharpR.svg",
+        "../dist/svg/released_sharp/G_sharpR.svg",
+        "../dist/svg/released_sharp/A_sharpR.svg",
+
+    ]
+    pressedSharp= [
+        "../dist/svg/pressed_sharp/A_sharpP.svg",
+        "../dist/svg/pressed_sharp/C_sharpP.svg",
+        "../dist/svg/pressed_sharp/D_sharpP.svg",
+        "../dist/svg/pressed_sharp/F_sharpP.svg",
+        "../dist/svg/pressed_sharp/G_sharpP.svg",
+
+        "../dist/svg/pressed_sharp/A_sharpP.svg",
+        "../dist/svg/pressed_sharp/C_sharpP.svg",
+        "../dist/svg/pressed_sharp/D_sharpP.svg",
+        "../dist/svg/pressed_sharp/F_sharpP.svg",
+        "../dist/svg/pressed_sharp/G_sharpP.svg",
+
+        "../dist/svg/pressed_sharp/A_sharpP.svg",
+        "../dist/svg/pressed_sharp/C_sharpP.svg",
+        "../dist/svg/pressed_sharp/D_sharpP.svg",
+        "../dist/svg/pressed_sharp/F_sharpP.svg",
+        "../dist/svg/pressed_sharp/G_sharpP.svg",
+
+        "../dist/svg/pressed_sharp/A_sharpP.svg",
+        "../dist/svg/pressed_sharp/C_sharpP.svg",
+        "../dist/svg/pressed_sharp/D_sharpP.svg",
+        "../dist/svg/pressed_sharp/F_sharpP.svg",
+        "../dist/svg/pressed_sharp/G_sharpP.svg",
+
+        "../dist/svg/pressed_sharp/A_sharpP.svg",
+        "../dist/svg/pressed_sharp/C_sharpP.svg",
+        "../dist/svg/pressed_sharp/D_sharpP.svg",
+        "../dist/svg/pressed_sharp/F_sharpP.svg",
+        "../dist/svg/pressed_sharp/G_sharpP.svg",
+        "../dist/svg/pressed_sharp/A_sharpP.svg",
+        "../dist/svg/pressed_sharp/C_sharpP.svg",
+        "../dist/svg/pressed_sharp/D_sharpP.svg",
+        "../dist/svg/pressed_sharp/F_sharpP.svg",
+        "../dist/svg/pressed_sharp/G_sharpP.svg",
+    ]
+    pressedFlat = [
+        "./svg/pressed_flat/A_flatP.svg",
+        "./svg/pressed_flat/C_flatP.svg",
+        "./svg/pressed_flat/D_flatP.svg",
+        "./svg/pressed_flat/F_flatP.svg",
+        "./svg/pressed_flat/G_flatP.svg",
+    ]
+
+}
+//dist for Keys
