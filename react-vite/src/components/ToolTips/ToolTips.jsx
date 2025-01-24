874e6f8a74987155e7fc774ce3067148b72bdb75
Shawn <sgiovanni1@yahoo.com>
2025-01-24 18:53:43 -0500

    drag select keys

---
+++ react-vite/src/components/ToolTips/ToolTips.jsx
@@ -0,0 +1,9 @@
+const ToolTips = ({ text }) => {
+    return (
+        <div className="tooltip">
+            <span className="tooltip-text">{text}</span>
+        </div>
+    )
+}
+
+export default ToolTips
