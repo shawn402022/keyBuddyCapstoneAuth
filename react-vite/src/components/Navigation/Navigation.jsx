a2910c4bbf1c111c3bd7f4fd1718782467a5c87f
Shawn <sgiovanni1@yahoo.com>
2025-01-24 17:14:17 -0500

    sign in prompt if your not logged on

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,19 +1,17 @@
-import { NavLink, useNavigate, Link} from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import CourseButton from "./CourseButton";
 import { useDispatch } from "react-redux";
 import { thunkLogout } from "../../redux/session";
 import ReviewButton from "./ReviewButton";
 import CreateReviewButton from "./CreateReviewButton";
-
-
-
 import "./Navigation.css";
 
 function Navigation() {
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
 
   const handleLogout = () => {
     dispatch(thunkLogout()).then(() => {
@@ -21,37 +19,44 @@ function Navigation() {
     });
   };
 
+  const handleProtectedRoute = (e, path) => {
+    if (!sessionUser) {
+      e.preventDefault();
+      navigate('/login');
+    }
+  };
 
   return (
     <div>
       <ul className="nav">
         <li>
-          <NavLink   to="/"><p className="home-icon">Home</p></NavLink>
+          <NavLink to="/"><p className="home-icon">Home</p></NavLink>
         </li>
         <li>
           <CourseButton />
         </li>
         <li>
           <p className="create-course-button">
-            <NavLink  to="/createCourse">Create Course</NavLink>
+            <NavLink to="/createCourse" onClick={(e) => handleProtectedRoute(e, '/createCourse')}>
+              Create Course
+            </NavLink>
           </p>
-
         </li>
         <li>
-          <ReviewButton />
+          <NavLink to="/reviews" onClick={(e) => handleProtectedRoute(e, '/reviews')}>
+            <ReviewButton />
+          </NavLink>
         </li>
         <li>
-          <CreateReviewButton />
+          <NavLink to="/reviews/new" onClick={(e) => handleProtectedRoute(e, '/reviews/new')}>
+            <CreateReviewButton />
+          </NavLink>
         </li>
         <li>
           <ProfileButton onLogout={handleLogout}/>
-
         </li>
       </ul>
-
-
     </div>
-
   );
 }
 
