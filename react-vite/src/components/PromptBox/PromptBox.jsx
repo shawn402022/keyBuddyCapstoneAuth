e59215e68a11cc1b9156bd7eb43223d2c99d9f86
Shawn <sgiovanni1@yahoo.com>
2025-01-26 08:55:00 -0500

    second  commit

---
+++ react-vite/src/components/PromptBox/PromptBox.jsx
@@ -0,0 +1,12 @@
+import "./PromptBox.css"
+
+const PromptBox = () => {
+    return (
+        <div className="prompt-container">
+            <div className="question">question</div>
+            <div className="answer">awnser</div>
+        </div>
+    )
+}
+
+export default PromptBox
