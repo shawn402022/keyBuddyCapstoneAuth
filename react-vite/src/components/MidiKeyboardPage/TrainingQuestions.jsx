51c27f34e326dea8cd42374fbdcde8f2502889ee
Shawn <sgiovanni1@yahoo.com>
2025-02-26 21:22:00 -0500

    just fixed chord major display in training questions

--- react-vite/src/components/MidiKeyboardPage/TrainingQuestions.jsx
+++ react-vite/src/components/MidiKeyboardPage/TrainingQuestions.jsx
@@ -1,10 +1,22 @@
 
 
+
 const TrainingQuestions = ({ course }) => {
+    // Function to transform chord notation in text
+    const formatChordsWithMajorSuffix = (text) => {
+        if (!text) return '';
+
+        // Regular expression to find chord notations
+        // This looks for patterns like 'C', 'G#', 'Bb', etc. that aren't followed by any chord quality
+        return text.replace(/\b([A-G][#b]?)(?!\w)/g, '$1M');
+    };
+
+    const formattedDetails = formatChordsWithMajorSuffix(course.details_of_course);
+
     return (
         <div className="training-questions">
             <h3>{course.course_name}</h3>
-            <p>{course.details_of_course}</p>
+            <p>{formattedDetails}</p>
             {/* Add more structured content display */}
         </div>
     );
