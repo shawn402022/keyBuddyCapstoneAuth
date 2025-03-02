91f80cf49b5773155126ed0ae21349bfb0a77e89
Shawn <sgiovanni1@yahoo.com>
2025-03-02 10:57:50 -0500

    still working on timer

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { useState, useEffect, useCallback, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import CourseButton from "./CourseButton";
@@ -8,79 +8,20 @@ import { thunkLogout } from "../../redux/session";
 import ReviewButton from "./ReviewButton";
 import { WebMidi } from "webmidi";
 import SoundModule from '../SoundModule/SoundModule';
-import { startTraining } from '../../redux/game';
 
 import "./Navigation.css";
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
   const trainingCourse = useSelector(state => state.userCourses.trainingCourse);
-  const [message, setMessage] = useState("");
-  const [targetKey, setTargetKey] = useState("");
-  const [isGameActive, setGameActive] = useState(false);
-  const [feedback, setFeedback] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const isMouseDownRef = useRef(false);
-
-  const generateNewChallenge = useCallback(() => {
-    const keys = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
-    const octaves = ['2', '3', '4', '5', '6', '7'];
-    const randomKey = keys[Math.floor(Math.random() * keys.length)];
-    const randomOctave = octaves[Math.floor(Math.random() * octaves.length)];
-    const fullNote = `${randomKey}${randomOctave}`;
-    setTargetKey(fullNote);
-    setMessage(`Please play ${fullNote} on the piano`);
-    setFeedback("");
-
-    setTimeout(() => {
-      SoundModule.playNote(fullNote);
-    }, 200);
-  }, []);
-
-  const checkNote = useCallback((playedNote) => {
-    if (!isGameActive) return;
-
-    if (playedNote === targetKey) {
-      setFeedback("Correct! Here's your next challenge!");
-      //Play the succes sound if you have one.
-      setTimeout(generateNewChallenge, 1000);
-    } else {
-      setFeedback(`Incorrect! Try again. You played ${playedNote}`);
-    }
-  }, [isGameActive, targetKey, generateNewChallenge]);
-
-
-
-  // Make sure this checkNote function is being called in both MIDI and mouse handlers
-useEffect(() => {
-  if (isGameActive) {
-    const setupMidi = async () => {
-      try {
-        await WebMidi.enable();
-        const myInput = WebMidi.getInputByName("KOMPLETE KONTROL A25 MIDI");
-        if (myInput) {
-          myInput.addListener("noteon", (e) => {
-            checkNote(e.note.identifier); // This will trigger the feedback
-          });
-        }
-      } catch (err) {
-        console.log("MIDI device not found or WebMidi not supported");
-      }
-    };
-    setupMidi();
-  }
-}, [isGameActive, checkNote]);
-
-
 
   const handleNotePlay = (note) => {
     SoundModule.playNote(note);
     updateKeyImage(note, true);
     setTimeout(() => updateKeyImage(note, false), 200);
-    if (isGameActive) {
-      checkNote(note);
-    }
   };
 
   const updateKeyImage = (note, isPressed) => {
@@ -145,18 +86,6 @@ useEffect(() => {
     };
   }, []);
 
-  const startKeyChallenge = () => {
-    dispatch(setGameActive(true));
-    generateNewChallenge();
-  };
-
-  const stopKeyChallenge = () => {
-    dispatch(setGameActive(false));
-    setMessage("");
-    setFeedback("");
-    setTargetKey("");
-  };
-
   const handleLogout = () => {
     dispatch(thunkLogout()).then(() => {
       navigate('/');
@@ -190,27 +119,7 @@ useEffect(() => {
             <li>
               <ReviewButton />
             </li>
-            <li>
-            {!isGameActive ? (
-              <button
-                  className='start-challenge'
-                  onClick={() => {
-                      startKeyChallenge()
-                      // Dispatch action to start training if there's a course
-                      if (trainingCourse) {
-                          dispatch(startTraining(trainingCourse))
-                      }
-                  }}
-              >
-                  Start Key Challenge
-              </button>
-          ) : (
-              <button className='stop-challenge' onClick={stopKeyChallenge}>
-                  Stop Key Challenge
-              </button>
-          )}
-          </li>
-        </>
+          </>
         ) : (
           <>
             <li>
@@ -223,18 +132,12 @@ useEffect(() => {
                 <span>Reviews</span>
               </div>
             </li>
-            <li>
-              <button onClick={handleProtectedAction} className="disabled-button">
-                Start Key Challenge
-              </button>
-            </li>
           </>
         )}
         <li>
           <ProfileButton onLogout={handleLogout}/>
         </li>
       </ul>
-
     </div>
   );
 }
