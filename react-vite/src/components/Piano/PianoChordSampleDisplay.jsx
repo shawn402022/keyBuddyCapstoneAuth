ade078bf4cab44b4317989b0bc55fdb801291a07
Shawn <sgiovanni1@yahoo.com>
2025-04-14 13:13:07 -0400

    before git pull main

--- react-vite/src/components/Piano/PianoChordSampleDisplay.jsx
+++ react-vite/src/components/Piano/PianoChordSampleDisplay.jsx
@@ -1,14 +1,9 @@
 import { usePianoContext } from "../../context/PianoContext"
-import { reformatData } from "../Data/ChordData"
 
-
-import noteUtil from "../../utils/noteUtil"
 
 const PianoChordSampleDisplay = () => {
 
-    console.log('REFORMAT',reformatData())
 
-    console.log(noteUtil.getEveryChord())
     return (
         <div>
 
