04fccabf12a8863e1e0d759d14dfe10d2c7db4e8
Shawn <sgiovanni1@yahoo.com>
2025-05-18 14:00:11 -0400

    added chord images

--- react-vite/src/components/MainPage/CreateImages.jsx
+++ react-vite/src/components/MainPage/CreateImages.jsx
@@ -1,11 +1,17 @@
 import { reformatData } from "../../utils/formatChordUtils"
 import AllChordsObjComplete from "../../data/Chord-Data-Complete";
+import PianoChordDetail2 from "../Piano/PianoChordDetail2";
 
 const CreateImages = () => {
     console.log(AllChordsObjComplete)
 
+
     return (
-        <div>Images
+        <div>
+            AllChordsObjComplete.map(item => (
+
+            ))
+
 
         </div>
     )
---
+++ react-vite/src/components/Piano/PianoChordDetail2.jsx
@@ -0,0 +1,13 @@
+import './PianoChordDetail.css' // Add a CSS file for styling
+
+
+
+const PianoChordDetail2 = ({id, key, name, notes, imageUrl}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PianoChordDetail2
